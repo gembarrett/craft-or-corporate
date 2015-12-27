cfa45ee99f41df5835d87e0d3bd63c0c468ab7f5
gembarrett <gem@gembarrett.com>
2015-12-26 23:05:24 -0500

    now toggles the hiding and showing of the list

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ var list = document.getElementById("see-list");
 // hide list element at first so if there's no javascript then site is still useable
 list.className += "hide-all";
 // when the text link is clicked on
-listClick.onclick = function() { showTheList(list) };
+listClick.onclick = function() { showHideTheList(list) };
 // add class which shows the list
-function showTheList(el) {
-  el.className += "show-all";
+function showHideTheList(el) {
+
+  // get current classnames
+  var className = ' ' + list.className + ' ';
+
+  // if showing, then hide
+  if ( ~className.indexOf(' show-all ') ) {
+    list.className = className.replace(' show-all ', ' hide-all ');
+  } else {
+    // else show
+    list.className += ' show-all';
+  }
 };
