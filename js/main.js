4f11d0bb52bd8c21db9b30f3f89a151c61c355f4
gembarrett <gem@gembarrett.com>
2015-12-26 22:53:22 -0500

    tweaked some colours and added show/hide for list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+// grab text link
+var listClick = document.getElementById("list-link");
+// grab list element
+var list = document.getElementById("see-list");
+// hide list element at first so if there's no javascript then site is still useable
+list.className += "hide-all";
+// when the text link is clicked on
+listClick.onclick = function() { showTheList(list) };
+// add class which shows the list
+function showTheList(el) {
+  el.className += "show-all";
+};
